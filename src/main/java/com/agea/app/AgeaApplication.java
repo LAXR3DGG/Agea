859496f10200bf1cc21b66e282d4b054cf8ca830
Marco_C141 <marcosolisc1311@gmail.com>
2025-09-10 18:58:43 -0600

    Base del proyecto

---
+++ src/main/java/com/agea/app/AgeaApplication.java
@@ -0,0 +1,13 @@
+package com.agea.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agea/app/AgeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agea.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
